5307ff6b8fa52bbaf14e58b0a3379e8575ee2d94
adeasepkurnia <adeasepkurnia@gmail.com>
2022-08-05 14:52:47 +0700

    pertama

---
+++ Kalkulator/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.no1 = new System.Windows.Forms.Button();
+            this.no2 = new System.Windows.Forms.Button();
+            this.no3 = new System.Windows.Forms.Button();
+            this.btambah = new System.Windows.Forms.Button();
+            this.bkurang = new System.Windows.Forms.Button();
+            this.no6 = new System.Windows.Forms.Button();
+            this.no5 = new System.Windows.Forms.Button();
+            this.no4 = new System.Windows.Forms.Button();
+            this.bkali = new System.Windows.Forms.Button();
+            this.no9 = new System.Windows.Forms.Button();
+            this.no8 = new System.Windows.Forms.Button();
+            this.no7 = new System.Windows.Forms.Button();
+            this.bbagi = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.no0 = new System.Windows.Forms.Button();
+            this.bdot = new System.Windows.Forms.Button();
+            this.bhasil = new System.Windows.Forms.Button();
+            this.bhapus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.White;
+            this.txt1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txt1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(0, 0);
+            this.txt1.Name = "txt1";
+            this.txt1.ReadOnly = true;
+            this.txt1.Size = new System.Drawing.Size(272, 26);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // no1
+            // 
+            this.no1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no1.Location = new System.Drawing.Point(12, 44);
+            this.no1.Name = "no1";
+            this.no1.Size = new System.Drawing.Size(58, 24);
+            this.no1.TabIndex = 1;
+            this.no1.Text = "1";
+            this.no1.UseVisualStyleBackColor = true;
+            this.no1.Click += new System.EventHandler(this.no1_Click);
+            // 
+            // no2
+            // 
+            this.no2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no2.Location = new System.Drawing.Point(76, 44);
+            this.no2.Name = "no2";
+            this.no2.Size = new System.Drawing.Size(58, 24);
+            this.no2.TabIndex = 2;
+            this.no2.Text = "2";
+            this.no2.UseVisualStyleBackColor = true;
+            this.no2.Click += new System.EventHandler(this.no2_Click);
+            // 
+            // no3
+            // 
+            this.no3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no3.Location = new System.Drawing.Point(140, 44);
+            this.no3.Name = "no3";
+            this.no3.Size = new System.Drawing.Size(58, 24);
+            this.no3.TabIndex = 3;
+            this.no3.Text = "3";
+            this.no3.UseVisualStyleBackColor = true;
+            this.no3.Click += new System.EventHandler(this.no3_Click);
+            // 
+            // btambah
+            // 
+            this.btambah.BackColor = System.Drawing.Color.Yellow;
+            this.btambah.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btambah.Location = new System.Drawing.Point(204, 44);
+            this.btambah.Name = "btambah";
+            this.btambah.Size = new System.Drawing.Size(58, 24);
+            this.btambah.TabIndex = 4;
+            this.btambah.Text = "+";
+            this.btambah.UseVisualStyleBackColor = false;
+            this.btambah.Click += new System.EventHandler(this.btambah_Click);
+            // 
+            // bkurang
+            // 
+            this.bkurang.BackColor = System.Drawing.Color.Yellow;
+            this.bkurang.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bkurang.Location = new System.Drawing.Point(204, 74);
+            this.bkurang.Name = "bkurang";
+            this.bkurang.Size = new System.Drawing.Size(58, 24);
+            this.bkurang.TabIndex = 8;
+            this.bkurang.Text = "-";
+            this.bkurang.UseVisualStyleBackColor = false;
+            this.bkurang.Click += new System.EventHandler(this.bkurang_Click);
+            // 
+            // no6
+            // 
+            this.no6.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no6.Location = new System.Drawing.Point(140, 74);
+            this.no6.Name = "no6";
+            this.no6.Size = new System.Drawing.Size(58, 24);
+            this.no6.TabIndex = 7;
+            this.no6.Text = "6";
+            this.no6.UseVisualStyleBackColor = true;
+            this.no6.Click += new System.EventHandler(this.no6_Click);
+            // 
+            // no5
+            // 
+            this.no5.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no5.Location = new System.Drawing.Point(76, 74);
+            this.no5.Name = "no5";
+            this.no5.Size = new System.Drawing.Size(58, 24);
+            this.no5.TabIndex = 6;
+            this.no5.Text = "5";
+            this.no5.UseVisualStyleBackColor = true;
+            this.no5.Click += new System.EventHandler(this.no5_Click);
+            // 
+            // no4
+            // 
+            this.no4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no4.Location = new System.Drawing.Point(12, 74);
+            this.no4.Name = "no4";
+            this.no4.Size = new System.Drawing.Size(58, 24);
+            this.no4.TabIndex = 5;
+            this.no4.Text = "4";
+            this.no4.UseVisualStyleBackColor = true;
+            this.no4.Click += new System.EventHandler(this.no4_Click);
+            // 
+            // bkali
+            // 
+            this.bkali.BackColor = System.Drawing.Color.Yellow;
+            this.bkali.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bkali.Location = new System.Drawing.Point(204, 104);
+            this.bkali.Name = "bkali";
+            this.bkali.Size = new System.Drawing.Size(58, 24);
+            this.bkali.TabIndex = 12;
+            this.bkali.Text = "*";
+            this.bkali.UseVisualStyleBackColor = false;
+            this.bkali.Click += new System.EventHandler(this.bkali_Click_1);
+            // 
+            // no9
+            // 
+            this.no9.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no9.Location = new System.Drawing.Point(140, 104);
+            this.no9.Name = "no9";
+            this.no9.Size = new System.Drawing.Size(58, 24);
+            this.no9.TabIndex = 11;
+            this.no9.Text = "9";
+            this.no9.UseVisualStyleBackColor = true;
+            this.no9.Click += new System.EventHandler(this.no9_Click);
+            // 
+            // no8
+            // 
+            this.no8.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no8.Location = new System.Drawing.Point(76, 104);
+            this.no8.Name = "no8";
+            this.no8.Size = new System.Drawing.Size(58, 24);
+            this.no8.TabIndex = 10;
+            this.no8.Text = "8";
+            this.no8.UseVisualStyleBackColor = true;
+            this.no8.Click += new System.EventHandler(this.no8_Click);
+            // 
+            // no7
+            // 
+            this.no7.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no7.Location = new System.Drawing.Point(12, 104);
+            this.no7.Name = "no7";
+            this.no7.Size = new System.Drawing.Size(58, 24);
+            this.no7.TabIndex = 9;
+            this.no7.Text = "7";
+            this.no7.UseVisualStyleBackColor = true;
+            this.no7.Click += new System.EventHandler(this.no7_Click);
+            // 
+            // bbagi
+            // 
+            this.bbagi.BackColor = System.Drawing.Color.Yellow;
+            this.bbagi.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bbagi.Location = new System.Drawing.Point(204, 134);
+            this.bbagi.Name = "bbagi";
+            this.bbagi.Size = new System.Drawing.Size(58, 26);
+            this.bbagi.TabIndex = 16;
+            this.bbagi.Text = "/";
+            this.bbagi.UseVisualStyleBackColor = false;
+            this.bbagi.Click += new System.EventHandler(this.bbagi_Click);
+            // 
+            // bc
+            // 
+            this.bc.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bc.Location = new System.Drawing.Point(140, 134);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(58, 26);
+            this.bc.TabIndex = 15;
+            this.bc.Text = "c";
+            this.bc.UseVisualStyleBackColor = true;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // no0
+            // 
+            this.no0.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.no0.Location = new System.Drawing.Point(76, 134);
+            this.no0.Name = "no0";
+            this.no0.Size = new System.Drawing.Size(58, 26);
+            this.no0.TabIndex = 14;
+            this.no0.Text = "0";
+            this.no0.UseVisualStyleBackColor = true;
+            this.no0.Click += new System.EventHandler(this.no0_Click);
+            // 
+            // bdot
+            // 
+            this.bdot.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bdot.Location = new System.Drawing.Point(12, 134);
+            this.bdot.Name = "bdot";
+            this.bdot.Size = new System.Drawing.Size(58, 26);
+            this.bdot.TabIndex = 13;
+            this.bdot.Text = ".";
+            this.bdot.UseVisualStyleBackColor = true;
+            this.bdot.Click += new System.EventHandler(this.bdot_Click);
+            // 
+            // bhasil
+            // 
+            this.bhasil.BackColor = System.Drawing.Color.LightGreen;
+            this.bhasil.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bhasil.Location = new System.Drawing.Point(60, 179);
+            this.bhasil.Name = "bhasil";
+            this.bhasil.Size = new System.Drawing.Size(60, 29);
+            this.bhasil.TabIndex = 17;
+            this.bhasil.Text = "=";
+            this.bhasil.UseVisualStyleBackColor = false;
+            this.bhasil.Click += new System.EventHandler(this.bhasil_Click);
+            // 
+            // bhapus
+            // 
+            this.bhapus.BackColor = System.Drawing.Color.LightGreen;
+            this.bhapus.Location = new System.Drawing.Point(140, 179);
+            this.bhapus.Name = "bhapus";
+            this.bhapus.Size = new System.Drawing.Size(64, 29);
+            this.bhapus.TabIndex = 19;
+            this.bhapus.Text = "Hapus";
+            this.bhapus.UseVisualStyleBackColor = false;
+            this.bhapus.Click += new System.EventHandler(this.bhapus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(272, 218);
+            this.Controls.Add(this.bhapus);
+            this.Controls.Add(this.bhasil);
+            this.Controls.Add(this.bbagi);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.no0);
+            this.Controls.Add(this.bdot);
+            this.Controls.Add(this.bkali);
+            this.Controls.Add(this.no9);
+            this.Controls.Add(this.no8);
+            this.Controls.Add(this.no7);
+            this.Controls.Add(this.bkurang);
+            this.Controls.Add(this.no6);
+            this.Controls.Add(this.no5);
+            this.Controls.Add(this.no4);
+            this.Controls.Add(this.btambah);
+            this.Controls.Add(this.no3);
+            this.Controls.Add(this.no2);
+            this.Controls.Add(this.no1);
+            this.Controls.Add(this.txt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "form 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button no1;
+        private System.Windows.Forms.Button no2;
+        private System.Windows.Forms.Button no3;
+        private System.Windows.Forms.Button btambah;
+        private System.Windows.Forms.Button bkurang;
+        private System.Windows.Forms.Button no6;
+        private System.Windows.Forms.Button no5;
+        private System.Windows.Forms.Button no4;
+        private System.Windows.Forms.Button bkali;
+        private System.Windows.Forms.Button no9;
+        private System.Windows.Forms.Button no8;
+        private System.Windows.Forms.Button no7;
+        private System.Windows.Forms.Button bbagi;
+        private System.Windows.Forms.Button bc;
+        private System.Windows.Forms.Button no0;
+        private System.Windows.Forms.Button bdot;
+        private System.Windows.Forms.Button bhasil;
+        private System.Windows.Forms.Button bhapus;
+    }
+}
+
---
+++ Kalkulator/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        string operasi;
+        double nilai1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void no1_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "1";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "1";
+            }
+
+        }
+
+        private void no2_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "2";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "2";
+            }
+        }
+
+        private void no3_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "3";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "3";
+            }
+        }
+
+        private void no4_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "4";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "4";
+            }
+        }
+
+        private void no5_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "5";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "5";
+            }
+        }
+
+        private void no6_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "6";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "6";
+            }
+        }
+
+        private void no7_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "7";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "7";
+            }
+        }
+
+        private void no8_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "8";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "8";
+            }
+        }
+
+        private void no9_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "9";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "9";
+            }
+        }
+
+        private void no0_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "0" && txt1.Text != null)
+            {
+                txt1.Text = "0";
+            }
+            else
+            {
+                txt1.Text = txt1.Text + "0";
+            }
+        }
+
+        private void bdot_Click(object sender, EventArgs e)
+        {
+          
+                txt1.Text = txt1.Text + ".";
+        }
+
+        private void btambah_Click(object sender, EventArgs e)
+        {
+            nilai1 = Convert.ToDouble(txt1.Text);
+            txt1.Text = "0";
+            operasi = "+";
+        }
+
+        private void bhasil_Click(object sender, EventArgs e)
+        {
+            double nilai2;
+            double hasil;
+            nilai2 = Convert.ToDouble(txt1.Text);
+
+            if (operasi == "+")
+            {
+                hasil = nilai1 + nilai2;
+                txt1.Text = Convert.ToString(hasil);
+            }
+            if (operasi == "-")
+            {
+                hasil = nilai1 - nilai2;
+                txt1.Text = Convert.ToString(hasil);
+            }
+            if (operasi == "/")
+            {
+                hasil = nilai1 / nilai2;
+                txt1.Text = Convert.ToString(hasil);
+            }
+            if (operasi == "*")
+            {
+                hasil = nilai1 * nilai2;
+                txt1.Text = Convert.ToString(hasil);
+            }
+        }
+
+    
+
+        private void bc_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "0";
+            operasi = "0";
+        }
+
+        private void bkali_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void bbagi_Click(object sender, EventArgs e)
+        {
+            nilai1 = Convert.ToDouble(txt1.Text);
+            txt1.Text = "0";
+            operasi = "/";
+        }
+
+        private void bkurang_Click(object sender, EventArgs e)
+        {
+            nilai1 = Convert.ToDouble(txt1.Text);
+            txt1.Text = "0";
+            operasi = "-";
+        }
+
+        private void bkali_Click_1(object sender, EventArgs e)
+        {
+            nilai1 = Convert.ToDouble(txt1.Text);
+            txt1.Text = "0";
+            operasi = "*";
+        }
+
+        private void bhapus_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text.Length > 0)
+                txt1.Text = txt1.Text.Remove(txt1.Text.Length - 1);
+        }
+    }
+}
